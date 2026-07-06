85725eb8297eb8307b3155fef8534fbe8a23e20b
Ubuntu <ubuntu@ip-172-31-43-134.ap-south-1.compute.internal>
2026-07-06 11:04:36 +0000

    initial commit

---
+++ monitor.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+###########
+#Author:Ajilash Edward A
+#Date:06/07/26
+#Version:1.0
+###########
+
+#This is the script for resource monitoring in AWS infra
+
+###########
+#Resource
+##########
+
+#Listing all EC2 instance
+echo "This is the IP Address:"
+
